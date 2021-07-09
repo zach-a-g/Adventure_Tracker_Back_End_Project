7d66715ab54268174d289f779d85eff3a9e7ffb2
Rodrigo Ruiz <ruiz.rodrigo46@yahoo.com>
2021-07-09 16:19:54 -0400

    finished renaming, switching info

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,16 @@
 const express = require('express');
 const router = express.Router();
 const ItineraryModel = require('../models/itineraryModel');
+const DateModel = require('../models/dateModel');
 
 router.get('/', async(req, res) => {
+    const user_id = req.session.user_id;
+    const theItinerary = await ItineraryModel.getItinerary(user_id);
     res.render('template', {
         locals: {
             title: 'Adventure Tracker | Home',
+            user_id,
+            data: theItinerary,
             is_logged_in: req.session.is_logged_in
         },
         partials: {
@@ -15,5 +20,19 @@ router.get('/', async(req, res) => {
         }
     })
 });
+
+router.get('/:id?', async(req, res) => {
+    const singleEvent = await DateModel.getSingleDate(req.params.id);
+    res.render('template', {
+        locals: {
+            title: 'DETAILS',
+            data: singleEvent,
+            is_logged_in: req.session.is_logged_in
+        },
+        partials: {
+            body: 'partials/info'
+        }
+    })
+})
 
 module.exports = router;
